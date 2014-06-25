ddefc479b3105350bafcd9c2f8c59b93c693b709
Toby <the-shadow-walker@outlook.com>
2014-06-25 11:43:26 +0100

    Update currency-converter.py

--- currency-converter.py
+++ currency-converter.py
@@ -1,6 +1,3 @@
-
-import requests
-
 a = raw_input('Enter currency to convert from?')
 a = a.upper()
 
@@ -12,20 +9,9 @@ c = float(raw_input('Enter value to convert?'))
 url = ('http://rate-exchange.appspot.com/currency?from=%s&to=%s&q=1') % (a, b)
 print url
 
-r = requests.get(url)
-print r.json()['v']
-
-print c*r.json()['v']
-
 urlalt = ('http://themoneyconverter.com/%s/%s.aspx') % (a, b)
 print urlalt
 
 #split and strip
 split1 = (' : 1 %s = ') % a
 strip1 = (' %s</h2>') % b
-
-ralt = requests.get(urlalt)
-d = float(ralt.text.split(split1)[1].split(strip1)[0].strip())
-print d
-
-print c * d
